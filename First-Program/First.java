b61a947003900eb8f2a5a78c993a724198ab39f4
PRAKASH-KAMALESAN <138619129+PRAKASH-KAMALESAN@users.noreply.github.com>
2026-03-01 15:00:06 +0530

    Add files via upload

---
+++ First-Program/First.java
@@ -0,0 +1,7 @@
+// Question: Write a Java program that prints "Hello, World!" to the console.
+
+public class First {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+}
